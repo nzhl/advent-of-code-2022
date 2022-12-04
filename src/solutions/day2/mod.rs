54d98f970875dd3301a85bed1107b901514347ed
Fred <nzhlfred@gmail.com>
2022-12-04 20:34:44 +0800

    feat: finish problem2 part2

--- src/solutions/day2/mod.rs
+++ src/solutions/day2/mod.rs
@@ -1,9 +1,6 @@
 use std::fs;
 use std::collections::HashMap;
 
-
-
-
 pub fn solve() {
     let mut map = HashMap::new();
     map.insert("A X", 1 + 3);
@@ -16,19 +13,38 @@ pub fn solve() {
     map.insert("C Y", 2 + 0);
     map.insert("C Z", 3 + 3);
 
+
+    let mut map2 = HashMap::new();
+    map2.insert("A X", 3 + 0);
+    map2.insert("A Y", 1 + 3);
+    map2.insert("A Z", 2 + 6);
+    map2.insert("B X", 1 + 0);
+    map2.insert("B Y", 2 + 3);
+    map2.insert("B Z", 3 + 6);
+    map2.insert("C X", 2 + 0);
+    map2.insert("C Y", 3 + 3);
+    map2.insert("C Z", 1 + 6);
+
     match fs::read_to_string("./input/day2/input") {
         Ok(content) => {
+            // part1 & part2
             let mut sum = 0;
+            let mut sum2 = 0;
             for line in content.split("\n") {
                 let case = line.trim();
                 if let Some(result) = map.get(case) {
                     sum += result;
                 }
+                if let Some(result) = map2.get(case) {
+                    sum2 += result;
+                }
             }
-            println!("Sum: {}", sum);
+            println!("Sum: {} {}", sum, sum2);
+
         }
         Err(err) => {
             eprintln!("Read input error:  {}!", err);
         }
     }
+
 }
