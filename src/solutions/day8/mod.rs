032cbf39bd1a86c9996efc3cbfe4b1eb345c7b2b
Fred <nzhlfred@gmail.com>
2022-12-14 12:54:53 +0800

    feat: solved problem8#part1

--- src/solutions/day8/mod.rs
+++ src/solutions/day8/mod.rs
@@ -1,44 +1,97 @@
 use std::fs;
 use std::vec::Vec;
+use std::collections::HashMap;
 
-pub fn solve() {
-    match fs::read_to_string("./input/day1/input") {
+fn part1() {
+    match fs::read_to_string("./input/day8/input") {
         Ok(content) => {
-            let mut max = 0;
-            let mut cur = 0;
-
-            let mut max3 = Vec::new();
-            for line in content.split("\n") {
-                if line.trim().len() == 0 {
-                    if cur > max {
-                        max = cur;
-                    }
-
-                    max3.push(cur);
-                    if max3.len() > 3 {
-                        let min = max3.iter().min().unwrap().to_owned();
-                        max3 = max3
-                            .iter()
-                            .filter_map(|&x| if x != min {Some(x)} else {None})
-                            .collect::<Vec<u32>>();
-                        while max3.len() < 3 {
-                            max3.push(min);
-                        }
-                    }
-
-                    cur = 0;
-
-
-                } else {
-                    cur += line.parse::<u32>().unwrap();
+            // build the grid in i32
+            let mut rows = vec![];
+            let mut columns: Vec<Vec<i32>> = vec![];
+            for row in content.split("\n").filter(|each|each.len() != 0).map(|line|{
+                return line.split("").filter_map(|each|each.parse::<i32>().ok()).collect::<Vec<_>>();
+            }) {
+                rows.push(row.clone());
+                for idx in 0..row.len() {
+                    let column = if columns.len() > idx {
+                        &mut columns[idx]
+                    } else {
+                        columns.push(vec![]);
+                        &mut columns[idx]
+                    };
+                    column.push(row[idx]);
                 }
             }
 
-            println!("Max: {}", max);
-            println!("Sum: {}", max3.iter().sum::<u32>());
+            let mut count = 0;
+            let mut map = HashMap::new();
+
+            fn get_map_key (i: usize, j: usize) -> String {
+                format!("[{}][{}]", i, j)
+            }
+
+
+            for i in 0..rows.len() {
+                let len = rows[i].len();
+                let mut cur_max_from_left = -1;
+                let mut cur_max_from_right = -1;
+                for j in 0..len {
+                    if rows[i][j] > cur_max_from_left {
+                        let map_key = get_map_key(i, j);
+                        if let None = map.get(&map_key) {
+                            map.insert(map_key, true);
+                            count += 1;
+                        }
+                        cur_max_from_left = rows[i][j];
+                    }
+
+                    if rows[i][len - j - 1] > cur_max_from_right {
+                        let map_key = get_map_key(i, len - j - 1);
+                        if let None = map.get(&map_key) {
+                            map.insert(map_key, true);
+                            count += 1;
+                        }
+                        cur_max_from_right = rows[i][len - j - 1];
+                    }
+                }
+            }
+
+
+            for i in 0..columns.len() {
+                let len = columns[i].len();
+                let mut cur_max_from_left = -1;
+                let mut cur_max_from_right = -1;
+                for j in 0..len {
+                    if columns[i][j] > cur_max_from_left {
+                        let map_key = get_map_key(j, i);
+                        if let None = map.get(&map_key) {
+                            map.insert(map_key, true);
+                            count += 1;
+                        }
+                        cur_max_from_left = columns[i][j];
+                    }
+
+                    if columns[i][len - j - 1] > cur_max_from_right {
+                        let map_key = get_map_key(len - j - 1, i);
+                        if let None = map.get(&map_key) {
+                            map.insert(map_key, true);
+                            count += 1;
+                        }
+                        cur_max_from_right = columns[i][len - j - 1];
+                    }
+                }
+            }
+
+            println!("count {}", count);
+
         }
         Err(err) => {
             eprintln!("Read input error:  {}!", err);
         }
     }
+
+}
+
+pub fn solve() {
+    part1();
 }
